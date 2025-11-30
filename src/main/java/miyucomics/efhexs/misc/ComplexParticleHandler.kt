c1d10053238adf88cde54d132cf28bbc22d76b30
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-11-30 18:19:45 -0500

    Cleanup time

--- src/main/java/miyucomics/efhexs/misc/ComplexParticleHandler.kt
+++
@@ -1,8 +0,0 @@
-package miyucomics.efhexs.misc
-
-import net.minecraft.network.PacketByteBuf
-import net.minecraft.particle.ParticleEffect
-
-interface ComplexParticleHandler {
-	fun produceParticleEffect(buf: PacketByteBuf): ParticleEffect
-}
